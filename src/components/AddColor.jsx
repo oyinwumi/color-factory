4bc46e8c314a3df566d95ffc585718037a143c30
Oyindamola Ogunknle <oyinwumi@gmail.com>
2022-12-14 19:54:18 +0600

    continuation

--- src/components/AddColor.jsx
+++ src/components/AddColor.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 export default function AddColor({inputValue, setInputValue , color, setColor ,colorValue, setColorValue}) {
-    // const[inputValue , setInputValue] = useState();
-    // const[color , setColor] = useState('');
-    // const[colorValue , setColorValue] = useState('')
+  
 
     const changeColor = (e) =>{
         setColor(e.target.value)
     }
-// const updateColor =() =>{
-    
-//     setColorValue(color)
-// }
+
+    let navigate = useNavigate();
+    const routeChange = () =>{
+      let path = `/`;
+      navigate(path)
+    }
+
   return (
  <div>
        <div className='form-container'>
@@ -21,7 +23,7 @@ export default function AddColor({inputValue, setInputValue , color, setColor ,c
         <input type="text" onChange={(e) => setInputValue(e.target.value)} /><br />
         <label htmlFor="color">Select Color</label>
         <input type="color"  id='select-color' onChange={changeColor}/><br />
-        <button onClick={(colorValue) =>setColorValue(colorValue)}>Add Color</button>
+        <button onClick={routeChange}>Add Color</button>
         </form> 
         <Link to="/">Go back to Home</Link>
     </div>
